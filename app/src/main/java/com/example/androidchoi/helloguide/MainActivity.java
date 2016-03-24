b313190b7bec29c164cce35b8b8d4e2fd367d591
choijaewoong <jwchoi0129@gmail.com>
2016-03-24 12:53:26 +0900

    Setting Toolbar  * 기본 색상 설정 (갈색톤)

--- app/src/main/java/com/example/androidchoi/helloguide/MainActivity.java
+++ app/src/main/java/com/example/androidchoi/helloguide/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.androidchoi.helloguide;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Setting Toolbar
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        //toolbar.setTitle(R.string.app_name);
+        toolbar.setTitleTextColor(ContextCompat.getColor(getBaseContext(),android.R.color.white));
+        setSupportActionBar(toolbar);
+
+        // List Attraction
     }
 }
