57b605fd1b1ad4c31ee8ccabf9f6a6303342ef7e
choijaewoong <jwchoi0129@gmail.com>
2016-03-26 02:03:45 +0900

    뷰 페이저 구현  * with TabLayout  * 페이저 어댑터 생성

--- app/src/main/java/com/example/androidchoi/helloguide/PlaceInfoActivity.java
+++ app/src/main/java/com/example/androidchoi/helloguide/PlaceInfoActivity.java
@@ -1,15 +1,22 @@
 package com.example.androidchoi.helloguide;
 
 import android.content.Intent;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.androidchoi.helloguide.Adapter.PlaceInfoPagerAdapter;
 import com.example.androidchoi.helloguide.model.PlaceData;
 
 public class PlaceInfoActivity extends AppCompatActivity {
 
     public static final String PLACEDATA = "placeData";
+
+    PlaceInfoPagerAdapter mPlaceInfoPagerAdapter;
+    ViewPager mViewPager;
+    TabLayout mTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +27,13 @@ public class PlaceInfoActivity extends AppCompatActivity {
         Intent intent = getIntent();
         PlaceData placeData = (PlaceData)intent.getSerializableExtra(PLACEDATA);
 
+        // 뷰페이저 설정
+//        mPlaceInfoPagerAdapter = new PlaceInfoPagerAdapter(getSupportFragmentManager(), fragments);
+//        mPlaceInfoPagerAdapter.setTabList(new String[]{"정보", "위치 검색"});
+//        mViewPager = (ViewPager)findViewById(R.id.view_pager);
+//        mViewPager.setAdapter(mPlaceInfoPagerAdapter);
+//        mTabLayout = (TabLayout)findViewById(R.id.sliding_tabs);
+//        mTabLayout.setupWithViewPager(mViewPager);
         Toast.makeText(PlaceInfoActivity.this, placeData.getName() +"/" +placeData.getContent(), Toast.LENGTH_SHORT).show();
     }
 }
