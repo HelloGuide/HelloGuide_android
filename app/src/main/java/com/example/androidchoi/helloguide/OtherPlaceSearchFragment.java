ccb1bb51e18b14479d576dfa793b972c3d687fd7
torerov <torerov39@gmail.com>
2016-04-01 11:38:50 +0900

    (OtherPlaceSearchFragment) google map 생성   * 주요 건물 명이 명시되어 나오지 않음.   * Tmap api 으로 변경 예정

--- app/src/main/java/com/example/androidchoi/helloguide/OtherPlaceSearchFragment.java
+++ app/src/main/java/com/example/androidchoi/helloguide/OtherPlaceSearchFragment.java
@@ -7,12 +7,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class OtherPlaceSearchFragment extends Fragment {
 
+    static final LatLng DEFAULT_POINT = new LatLng( 37.56, 126.97);
+    private GoogleMap map;
 
     public OtherPlaceSearchFragment() {
         // Required empty public constructor
@@ -23,7 +32,14 @@ public class OtherPlaceSearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_other_place_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_other_place_search, container, false);
+
+        getFragmentManager().findFragmentById(R.id.map);
+        map = ((MapFragment)getActivity().getFragmentManager().findFragmentById(R.id.map)).getMap();
+        Marker seoul = map.addMarker(new MarkerOptions().position(DEFAULT_POINT).title("Seoul"));
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(DEFAULT_POINT, 15));
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        return view;
     }
 
 
